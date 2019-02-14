c21866b8cd5730ca69cecea3657b400c200abdee
James Farrell <jamesfarrellmusic@gmail.com>
2019-02-14 16:13:19 -0600

    new master merged

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,10 @@
 // import eventsApiManager from "./events/eventsApiManager";
+import newsHTMLFactory from "./newsFeed/newsHTMLFactory"
+// import printToDOM from "./welcome/printToDOM";
+// import welcomeForms from "./welcome/welcomeForms";
 
-import printToDOM from "./welcome/printToDOM";
-import welcomeForms from "./welcome/welcomeForms";
-
-printToDOM(welcomeForms.loginForm, "#welcomeForm")
+// printToDOM(welcomeForms.loginForm, "#welcomeForm")
 
 // eventsApiManager(r => console.log(r))
+
+newsHTMLFactory()
