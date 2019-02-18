af0d50e84650b85dc0eab7d10961e164cb99632e
James Farrell <jamesfarrellmusic@gmail.com>
2019-02-18 15:10:48 -0600

    fixed event listeners from overloading

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,10 @@ import welcome from "./welcome/welcome";
 import welcomeForms from "./welcome/welcomeForms";
 import welcomeEventHandlers from "./welcome/welcomeEventHandler"
 import navbarBuilder from "./navbar/navbarHTML";
-import moment from "moment"
+import newsEventListener from "./newsFeed/newsEventListenerHandler"
 
 navbarBuilder()
 welcome.welcome(welcomeForms.loginForm)
 welcomeEventHandlers.all()
+newsEventListener.inputContainer()
+newsEventListener.articleContainer()
