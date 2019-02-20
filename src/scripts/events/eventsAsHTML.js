537573f7703e3607a00bc1c096e30260f73b94c5
Brian Neal <brianbneal@gmail.com>
2019-02-20 14:44:07 -0600

    branch finished

--- src/scripts/events/eventsAsHTML.js
+++ src/scripts/events/eventsAsHTML.js
@@ -3,11 +3,11 @@ const eventsAsHTML = (eventsArray) => {
     eventsArray.forEach(event => {
         html += `
             <div class="eventCard articleContainer" id="eventCard--${event.id}">
-                <h2 class="eventHeader">${event.name}</h2>
+                <div class="eventHeader">${event.name}</div>
                 <div class="eventLocation">${event.location}</div>
                 <div class="eventDate">${event.date}</div>
-                <button id="editEvent--${event.id}" class="newsCardButton">Edit</button>
-                <button id="deleteEvent--${event.id}" class="newsCardButton">Delete</button>
+                <button id="editEvent--${event.id}" class="newsCardButton eventCardEdit">Edit</button>
+                <button id="deleteEvent--${event.id}" class="newsCardButton eventCardDelete">Delete</button>
             </div>
         `
     });
